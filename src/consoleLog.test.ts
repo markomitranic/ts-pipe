4d5828cb0bcc83fcf4f82f99173faf878f3d5280
Marko Mitranic <marko.mitranic@gmail.com>
2023-07-09 19:13:30 +0200

    Rename console utility

--- src/consoleLog.test.ts
+++ src/consoleLog.test.ts
@@ -1,12 +1,12 @@
-import { pipe } from "./index";
-import { console_log } from "./utilities";
+import consoleLog from "./consoleLog";
+import pipe from "./pipe";
 
 test("console_log returns the argument", () => {
   jest.spyOn(console, "log").mockImplementation(() => {
     return null;
   }); // shhh...
-  expect(console_log("1")).toBe("1");
+  expect(consoleLog("1")).toBe("1");
 });
 
 test("console_log works within a pipe", () =>
-  pipe("1", console_log, (__) => expect(__).toBe("1")));
+  pipe("1", consoleLog, (__) => expect(__).toBe("1")));
